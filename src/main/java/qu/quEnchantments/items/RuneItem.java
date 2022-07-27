3442e5bfd2d09abd485dc19e703ed489bd7a108f
BuildTools <jonathanhart1114@gmail.com>
2022-07-26 22:48:45 -0400

    Rewrote tick method

--- src/main/java/qu/quEnchantments/items/RuneItem.java
+++ src/main/java/qu/quEnchantments/items/RuneItem.java
@@ -1,20 +1,17 @@
 package qu.quEnchantments.items;
 
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tag.ItemTags;
 import net.minecraft.world.World;
 import qu.quEnchantments.enchantments.ModEnchantments;
+import qu.quEnchantments.util.IItemStack;
 
 public class RuneItem extends Item {
 
@@ -37,28 +34,23 @@ public class RuneItem extends Item {
     public boolean isDamageable() {
         return false;
     }
-
+    
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if (entity instanceof LivingEntity livingEntity) {
-            if (entity.age % 20 == 0 && stack.hasEnchantments()) {
-                if ((selected || livingEntity.getOffHandStack() == stack)
-                        && !(entity instanceof PlayerEntity player && player.getAbilities().creativeMode)) {
-                    if (stack.getDamage() < stack.getMaxDamage()) {
-                        if (stack.getOrCreateNbt().contains("Corrupted")) {
-                            int i = EnchantmentHelper.getLevel(ModEnchantments.OMEN_OF_IMMUNITY, stack);
-                            if (stack.getDamage() + 6 - i < stack.getMaxDamage()) {
-                                stack.setDamage(Math.min(stack.getDamage() + 6 - i, stack.getMaxDamage()));
-                            } else {
-                                stack.damage(6 - i, livingEntity, e -> e.sendEquipmentBreakStatus(selected ? EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND));
-                            }
-                        } else {
-                            stack.setDamage(Math.min(stack.getDamage() + stack.getEnchantments().size(), stack.getMaxDamage()));
-                        }
-                    }
-                } else if (stack.getDamage() > 0 && !stack.getOrCreateNbt().contains("Corrupted")) {
-                    stack.setDamage(Math.max(0, stack.getDamage() - 1));
-                }
+        if (!stack.hasEnchantments() || entity.age % 20 != 0) return;
+        int bl = ((IItemStack)(Object)stack).corruptedLevel();
+        if (!selected && !(entity instanceof LivingEntity livingEntity && livingEntity.getOffHandStack() == stack) || (entity instanceof PlayerEntity player && player.getAbilities().creativeMode)) {
+            if (bl == 0) {
+                stack.setDamage(Math.max(0, stack.getDamage() - 1));
+            }
+            return;
+        }
+        if (bl == 0) {
+            stack.setDamage(Math.min(stack.getMaxDamage(), stack.getDamage() + stack.getEnchantments().size()));
+        } else {
+            stack.setDamage(Math.min(stack.getMaxDamage() - 1, stack.getDamage() + 6 - bl));
+            if (stack.getDamage() == stack.getMaxDamage() - 1) {
+                stack.damage(500, (LivingEntity) entity, e -> e.sendEquipmentBreakStatus(selected ? EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND));
             }
         }
     }
