91261831a6bdc29f26dc43317f08b8afe38d31ff
Quplet <jonathanhart1114@gmail.com>
2024-06-27 18:17:55 -0400

    Unused import

--- src/main/java/qu/quEnchantments/enchantments/effect/entity/NightbloodDrainEnchantmentEffect.java
+++ src/main/java/qu/quEnchantments/enchantments/effect/entity/NightbloodDrainEnchantmentEffect.java
@@ -11,7 +11,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import qu.quEnchantments.enchantments.QuEnchantmentHelper;
 import qu.quEnchantments.util.interfaces.IEntity;
 
 public record NightbloodDrainEnchantmentEffect(float drainRate) implements EnchantmentEntityEffect {
